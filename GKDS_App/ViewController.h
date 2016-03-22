bea5e15f3c0e6e1e1589d87b3dc9fbc3a850747b
Mr.Wang <wang@wangdemacbook-pro.local>
2016-03-22 21:46:12 +0800

    Initial Commit

---
+++ GKDS_App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GKDS_App
+//
+//  Created by wang on 16/3/22.
+//  Copyright © 2016年 wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GKDS_App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GKDS_App
+//
+//  Created by wang on 16/3/22.
+//  Copyright © 2016年 wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
